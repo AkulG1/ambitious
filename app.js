566e2e3aacb3a416465367d832bd8021e6e7c899
AkulG1 <32958511+AkulG1@users.noreply.github.com>
2018-08-01 00:32:32 +0530

    Your commit message here

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var app = express();
+var url = require('url');
+
 var port = process.env.PORT || 3000;
 var bodyParser = require('body-parser')
 app.use('/assets', express.static(__dirname + '/public'));
@@ -9,33 +11,52 @@ app.get('/', function (req, res) {
     res.render('mainpage');
 });
 
-app.get('/login',function(req,res){
+app.get('/login', function (req, res) {
     res.render('login');
 })
 
-app.get('/signup',function(req,res){
+app.get('/signup', function (req, res) {
     res.render('signup');
 })
 
-app.get('/bitsat',function(req,res){
+app.get('/bitsat', function (req, res) {
     res.render('bitsat');
 })
 
-app.get('/aieee',function(req,res){
+app.get('/aieee', function (req, res) {
     res.render('aieee');
 })
 
-app.get('/ip',function(req,res){
+app.get('/ip', function (req, res) {
     res.render('ip');
 })
 
-app.get('/jeemain',function(req,res){
+app.get('/jeemain', function (req, res) {
     res.render('jeemain');
 })
 
-app.get('/jeeadvanced',function(req,res){
+app.get('/jeeadvanced', function (req, res) {
     res.render('jeeadvanced');
 })
+
+app.get('/jeemain/:year', function (req, res) {
+    res.render(__dirname + '/views/jee_main/' + req.params.year);
+});
+
+app.get('/jeemain/:year/:paper', function (req, res) {
+    var filename = req.params.paper.substr(0, 2) + 'April' + req.params.year
+    var mor = req.params.paper.indexOf('Morning')
+    var eve = req.params.paper.indexOf('Evening')
+    if (mor !== -1) {
+        filename = filename + req.params.paper.substr(mor, req.params.paper.length)
+
+    }
+    if (eve !== -1) {
+        filename = filename + req.params.paper.substr(eve, req.params.paper.length);
+    }
+    res.download(__dirname + '/downloads/jee_main/' + req.params.year + '/' + filename+'.pdf')
+});
+
 
 
 app.listen(port);
