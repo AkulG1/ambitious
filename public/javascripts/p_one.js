612b0e59a9284357cc0aaa97c584dba96806801f
AkulG1 <32958511+AkulG1@users.noreply.github.com>
2018-08-27 18:10:05 +0530

    Editing looks after this

--- public/javascripts/p_one.js
+++ public/javascripts/p_one.js
@@ -1,5 +1,5 @@
 console.log("Connected");
-$('[data-toggle="slide-collapse"]').on('click', function() {
+$('[data-toggle="slide-collapse"]').on('click', function () {
   $navMenuCont = $($(this).data('target'));
   $navMenuCont.animate({
     'width': 'toggle'
@@ -19,18 +19,18 @@ function openNav() {
   $('.overlay').show();
 }
 
-$(document.body).click(function(evt){
-  var snav=$("#mySidenav");
-  var itselements=$(".nottarget");
-  if (!snav.is(evt.target) && !itselements.is(evt.target) && window.matchMedia("(max-width: 768px)").matches){
+$(document.body).click(function (evt) {
+  var snav = $("#mySidenav");
+  var itselements = $(".nottarget");
+  if (!snav.is(evt.target) && !itselements.is(evt.target) && window.matchMedia("(max-width: 768px)").matches) {
     document.getElementById("mySidenav").style.width = 0;
     $('.overlay').hide();
-}
+  }
 });
 
 function closeNav() {
   document.getElementById("mySidenav").style.width = "0";
   $('.overlay').hide();
-  
+
 }
 
