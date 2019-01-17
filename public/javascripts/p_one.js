cfb707b66c3a5eb7d4522de336fb3391831cb2cd
AkulG1 <32958511+AkulG1@users.noreply.github.com>
2019-01-17 20:25:15 +0530

    JS bug resolved

--- public/javascripts/p_one.js
+++ public/javascripts/p_one.js
@@ -2,40 +2,40 @@ console.log("Connected");
 
 $(document).ready(function () {
 
-$('[data-toggle="slide-collapse"]').on('click', function () {
-  $navMenuCont = $($(this).data('target'));
-  $navMenuCont.animate({
-    'width': 'toggle'
-  }, 350);
-});
+  $('[data-toggle="slide-collapse"]').on('click', function () {
+    $navMenuCont = $($(this).data('target'));
+    $navMenuCont.animate({
+      'width': 'toggle'
+    }, 350);
+  });
 
 
-function openNav() {
+  window.openNav = function () {
 
-  if (window.matchMedia("(min-width: 641px)").matches) {
-    document.getElementById("mySidenav").style.width = "230px";
+    if (window.matchMedia("(min-width: 641px)").matches) {
+      document.getElementById("mySidenav").style.width = "230px";
 
-  } else {
-    document.getElementById("mySidenav").style.width = "180px";
+    } else {
+      document.getElementById("mySidenav").style.width = "180px";
+    }
+    document.getElementById("mySidenav").style.boxShadow = "0.6px 0.6px 10px black";
+    $('.overlay').show();
   }
-  document.getElementById("mySidenav").style.boxShadow = "0.6px 0.6px 10px black";
-  $('.overlay').show();
-}
 
-$(document.body).click(function (evt) {
-  var snav = $("#mySidenav");
-  var itselements = $(".nottarget");
-  if (!snav.is(evt.target) && !itselements.is(evt.target) && window.matchMedia("(max-width: 768px)").matches) {
-    document.getElementById("mySidenav").style.width = 0;
+  $(document.body).click(function (evt) {
+    var snav = $("#mySidenav");
+    var itselements = $(".nottarget");
+    if (!snav.is(evt.target) && !itselements.is(evt.target) && window.matchMedia("(max-width: 768px)").matches) {
+      document.getElementById("mySidenav").style.width = 0;
+      $('.overlay').hide();
+    }
+  });
+
+  window.closeNav = function () {
+    document.getElementById("mySidenav").style.width = "0";
     $('.overlay').hide();
+
   }
-});
-
-function closeNav() {
-  document.getElementById("mySidenav").style.width = "0";
-  $('.overlay').hide();
-
-}
 });
 
 
