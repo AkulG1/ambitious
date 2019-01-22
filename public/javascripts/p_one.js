33bcaa3fd3de518fa2ac0b972270cf673580ddfa
AkulG1 <32958511+AkulG1@users.noreply.github.com>
2019-01-23 03:05:33 +0530

    Idk tbh

--- public/javascripts/p_one.js
+++ public/javascripts/p_one.js
@@ -1,27 +1,15 @@
 console.log("Connected");
 
 $(document).ready(function () {
-
-  // $('[data-toggle="slide-collapse"]').on('click', function () {
-  //   $navMenuCont = $($(this).data('target'));
-  //   $navMenuCont.animate({
-  //     'width': 'toggle'
-  //   }, 350);
-  // });
-
-
   window.openNav = function () {
-
     if (window.matchMedia("(min-width: 641px)").matches) {
       document.getElementById("mySidenav").style.width = "230px";
-
     } else {
       document.getElementById("mySidenav").style.width = "180px";
     }
     document.getElementById("mySidenav").style.boxShadow = "0.6px 0.6px 10px black";
     $('.overlay').show();
   }
-
   $(document.body).click(function (evt) {
     var snav = $("#mySidenav");
     var itselements = $(".nottarget");
@@ -30,11 +18,9 @@ $(document).ready(function () {
       $('.overlay').hide(); 
     }
   });
-
   window.closeNav = function () {
     document.getElementById("mySidenav").style.width = "0";
     $('.overlay').hide();
-
   }
   window.onscroll = function () { scrollFunction() };
 
